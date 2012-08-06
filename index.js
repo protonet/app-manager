53dfb80b55e0623ab6a941b0cee7ccf8c2675166
Daniel Lamando <me@danopia.net>
2012-08-06 17:43:59 +0200

    Clean up the code to handle Heroku Buildpacks
    Some semantics changes. Don't need to locate magic anymore because
    sub-directory magic won't be support, except .zip which will demand that
    the code be in a subdirectory named after the .zip

--- index.js
+++ index.js
@@ -4,35 +4,41 @@ var path  = require('path'),
     
     httpd = require('./httpd');
 
-var packagers = [require('./packagers/node'), require('./packagers/rack')];
-
 var storeRoot = path.join(path.dirname(module.filename), 'store');
-if (!fs.existsSync(storeRoot)) require('fs').mkdirSync(storeRoot);
-  
-var appRoot = path.join(storeRoot, 'apps');
-if (!fs.existsSync(appRoot)) require('fs').mkdirSync(appRoot);
-  
-var tmpRoot = path.join(storeRoot, 'tmp');
-if (!fs.existsSync(tmpRoot)) require('fs').mkdirSync(tmpRoot);
+var appRoot   = path.join(storeRoot, 'apps');
+var packRoot  = path.join(storeRoot, 'buildpacks');
 
-// Create a unique temp directory to work in for unknown apps
-function getTmp(callback) {
-  spawn('mktemp', ['-d', '--tmpdir=' + tmpRoot, 'XXXXXXXXXX']).stdout.
-    on('data', function (data) {
-      callback(data.trim());
-    }).setEncoding('utf8');
+function createName (name, callback) {
+  var appPath = path.join(appRoot, name);
+  fs.exists(appPath, function (exists) {
+    if (exists) {
+      createName(name + '-', callback);
+    } else {
+      fs.mkdir(appPath, function (err) {
+        callback(appPath);
+      });
+    }
+  });
 }
 
-function fetch(uri, target, callback) {
+function fetch (uri, callback) {
   console.log('Fetching ' + uri);
   
   if (uri.match(/^git(@|:\/\/)/)) {
     console.log('Remote git repo identified, cloning');
-    spawn('git', ['clone', uri, target]).on('exit', callback);
+    createName(path.basename(uri, '.git'), function (appPath) {
+      spawn('git', ['clone', uri, path.join(appPath, 'src')]).on('exit', function () {
+        callback(appPath);
+      });
+    }
     
   } else if (uri.match(/^\/.+\.zip$/i)) {
     console.log('Local zip archive identified, extracting');
-    spawn('unzip', [uri, '-d', target]).on('exit', callback);
+    createName(path.basename(uri, '.zip'), function (appPath) {
+      spawn('unzip', [uri, '-d', appPath, path.join(path.basename(uri, '.zip'), '*')]).on('exit', function () {
+        callback(appPath);
+      });
+    }
     
   } else {
     console.log('I have no idea how to grab ' + uri);
@@ -40,105 +46,27 @@ function fetch(uri, target, callback) {
   }
 }
 
-function getMagicFiles() {
-  var magicFiles = [];
-  
-  packagers.forEach(function (packager) {
-    magicFiles = magicFiles.concat(packager.magics);
-  });
-  
-  return magicFiles;
-}
-
-function checkDirectoriesForMagics(magics, dirs, callback) {
-  var foundMagics = [];
-  var foundChildren = [];
-  
-  dirs.forEach(function (dir) {
-    fs.readdir(dir, function (readErr, files) {
-      files.forEach(function (file) {
-        var filePath = path.join(dir, file);
+function installFrom (uri, callback) {
+  fetch(uri, function (appPath) {
+    console.log('Fetched into ' + appPath);
+    
+    locate(rawPath, srcPath, function (packager) {
+      var slug = packager.getSlug(srcPath),
+          appPath = path.join(appRoot, slug);
+      
+      console.log('Packaging an ' + packager.name + ' app called ' + slug + ' at ' + srcPath);
+      
+      packager.assemble(srcPath, function () {
+        console.log('Assembly complete of ' + slug);
         
-        fs.stat(filePath, function (statErr, stats) {
-          if (stats.isFile()) {
-            if (magics.indexOf(file) >= 0) {
-              foundMagics.push(filePath);
-            }
-          } else if (stats.isDirectory()) {
-            foundChildren.push(filePath);
-          }
+        fs.rename(srcPath, appPath, function () {
+          console.log(slug + ' is now in place at ' + appPath);
           
-          files.splice(files.indexOf(file), 1);
-          
-          if (files.length == 0) {
-            dirs.splice(dirs.indexOf(dir), 1);
+          fs.writeFile(path.join(appPath, '.packager'), packager.id, 'utf8', function () {
+            console.log('Associated ' + slug + ' with the ' + packager.name + ' engine');
+            console.log(slug + ' successfully assemblied and ready to roll.');
             
-            if (dirs.length == 0) {
-              callback(foundMagics, foundChildren);
-            }
-          }
-        });
-      });
-    });
-  });
-}
-
-function locate(raw, target, callback) {
-  var magicFiles = getMagicFiles();
-  
-  var checkBack = function (foundMagics, childDirs) {
-    if (foundMagics.length > 0) {
-      var magic = foundMagics[0];
-      var packager;
-      
-      packagers.forEach(function (thisPackager) {
-        if (thisPackager.magics.indexOf(path.basename(magic)) >= 0)
-          packager = thisPackager;
-      });
-      
-      console.log('Found a ' + packager.name + ' magic file at ' + magic);
-      
-      fs.rename(path.dirname(magic), target, function () {
-        callback(packager);
-        
-        // TODO: rm -r raw
-      });
-    }
-    
-    checkDirectoriesForMagics(magicFiles, childDirs, checkBack);
-  }
-  
-  checkBack([], [raw]);
-}
-
-function installFrom(uri, callback) {
-  getTmp(function (tmp) {
-    console.log('Secured ' + tmp + ' to work in');
-    
-    var rawPath = path.join(tmp, 'raw'),
-        srcPath = path.join(tmp, 'src');
-    
-    fetch(uri, rawPath, function () {
-      console.log('Fetched into ' + rawPath);
-      
-      locate(rawPath, srcPath, function (packager) {
-        var slug = packager.getSlug(srcPath),
-            appPath = path.join(appRoot, slug);
-        
-        console.log('Packaging an ' + packager.name + ' app called ' + slug + ' at ' + srcPath);
-        
-        packager.assemble(srcPath, function () {
-          console.log('Assembly complete of ' + slug);
-          
-          fs.rename(srcPath, appPath, function () {
-            console.log(slug + ' is now in place at ' + appPath);
-            
-            fs.writeFile(path.join(appPath, '.packager'), packager.id, 'utf8', function () {
-              console.log('Associated ' + slug + ' with the ' + packager.name + ' engine');
-              console.log(slug + ' successfully assemblied and ready to roll.');
-              
-              callback(slug);
-            });
+            callback(slug);
           });
         });
       });
