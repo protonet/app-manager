aba2b3833d422f4865f800c901200b2e492af7a1
Daniel Lamando <me@danopia.net>
2012-08-14 18:00:46 +0200

    First dynamic RPC call: detect

--- index.js
+++ index.js
@@ -6,16 +6,13 @@ var path  = require('path'),
     rpc   = require('./rpc'),
     httpd = require('./httpd'),
     packs = require('./buildpack'),
+    fetch = require('./fetch'),
 
     storeRoot = path.join(path.dirname(module.filename), 'store'),
     appRoot   = path.join(storeRoot, 'apps');
 
 packs.storePath = path.join(storeRoot, 'buildpacks');
 packs.maintainStore();
-
-packs.detect('/home/danopia/Code/protonet/app-manager/store/apps/node-example', function (pack, name) {
-  console.log(pack, name);
-});
 
 function createName (name, callback) {
   var appPath = path.join(appRoot, name);
@@ -27,52 +24,6 @@ function createName (name, callback) {
         callback(appPath);
       });
     }
-  });
-}
-
-function installFrom (uri, callback) {
-  fetch(uri, function (appPath) {
-    console.log('Fetched into ' + appPath);
-    
-    locate(rawPath, srcPath, function (packager) {
-      var slug = packager.getSlug(srcPath),
-          appPath = path.join(appRoot, slug);
-      
-      console.log('Packaging an ' + packager.name + ' app called ' + slug + ' at ' + srcPath);
-      
-      packager.assemble(srcPath, function () {
-        console.log('Assembly complete of ' + slug);
-        
-        fs.rename(srcPath, appPath, function () {
-          console.log(slug + ' is now in place at ' + appPath);
-          
-          fs.writeFile(path.join(appPath, '.packager'), packager.id, 'utf8', function () {
-            console.log('Associated ' + slug + ' with the ' + packager.name + ' engine');
-            console.log(slug + ' successfully assembled and ready to roll.');
-            
-            callback(slug);
-          });
-        });
-      });
-    });
-  });
-}
-
-function startApp (slug, callback) {
-  var port = httpd.reservePort(slug);
-  var appPath = path.join(appRoot, slug);
-  
-  fs.readFile(path.join(appPath, '.packager'), 'utf8', function (err, packagerName) {
-    var packager;
-    packagers.forEach(function (thisPackager) {
-      if (thisPackager.id == packagerName)
-        packager = thisPackager;
-    });
-    
-    packager.start(appPath, slug, port);
-    
-    // TODO: wait for confirmation from packager?
-    callback(packager, port);
   });
 }
 
@@ -99,14 +50,25 @@ db.connect(function () {
   db.conn.query('SELECT * FROM `app-manager`.`apps`').on('result', function(row) {
     console.log(row);
   });*/
+
+  // detect "path":"/home/danopia/Code/protonet/app-manager/store/apps/node-example"
+  var obj = {
+    detect: function (params, callback) {
+      packs.detect(params.path, function (pack, name) {
+        console.log(pack, name);
+        callback(null, [pack, name.slice(0, name.length - 1)]);
+      });
+    },
+  };
   
-  var obj = {};
   obj.hello = function (params, callback) {
     callback(null, 'Hello, world!');
   };
+  
   obj.help = function (params, callback) {
     callback(null, 'The only valid command is hello.');
   };
+  
   rpc.start(obj);
 });
 
