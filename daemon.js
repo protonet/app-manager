70583e6af3dcbce26fd174e1f280d2c6d6535f9f
Daniel Lamando <me@danopia.net>
2013-02-22 16:21:59 +0100

    Silent the daemonized process

--- daemon.js
+++ daemon.js
@@ -4,7 +4,7 @@ var fork = require('child_process').fork,
     target = path.dirname(module.filename);
 
 console.log('Starting daemon process...');
-var daemon = fork(target, ['daemon']);
+var daemon = fork(target, ['daemon'], {silent: true});
 daemon.on('message', function (m) {
   console.log('Daemon started.');
   process.exit();
